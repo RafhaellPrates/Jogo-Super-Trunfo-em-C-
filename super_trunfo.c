5b97e401205685be78197bcafb6ec48a5b8a5a47
Rafhael Prates <fellepy960@gmail.com>
2025-10-31 22:20:55 -0300

    Desafio nivel aventureiro

--- super_trunfo.c
+++ super_trunfo.c
@@ -8,6 +8,8 @@ int main(){
     float AreaEmKmCidade1;
     float PIBCidade1;
     int NumerosDePontosTuristicosCidade1;
+    float DensidadePopulacionalcidade1;
+    float PIBpercapitacidade1;
 
     // Segunda carta: B
     char codigoDaCarta2 []= "B01";
@@ -16,18 +18,20 @@ int main(){
     float AreaEmKmCidade2;
     float PIBCidade2;
     int NumerosDePontosTuristicosCidade2;
+    float DensidadePopulacionalcidade2;
+    float PIBpercapitacidade2;
 
-    printf("----Bem vindo ao cadrastro de cartas!!----\n");
+    printf("----Bem vindo ao cadastro de cartas!!----\n");
     printf("------------------------------------------\n\n");
 
     // carta A
 
+    // a especificção %[^\n] serve para salvar nomes compostos
+
     printf("Vamos começar com a sua primeira carta, ela receberá o codigo de especificação: A01. \n\nDigite qual será o nome da sua cidade: \n");
-    scanf("%s", &nomeDaCidade1 );
+    scanf("%[^\n]", nomeDaCidade1 );
 
-// OBSERVAÇÕES: COLOQUEI A POPULAÇÃO COMO DO TIPO FLOAT POR QUE INTEIRO SO NÃO ACEITARIA NÚMEROS QUEBRADOS.
-
-    printf("Qual é a população de abitantes (Em Milhões) de %s ?\n",nomeDaCidade1 );
+    printf("Qual é a população de habitantes (Em Milhões) de %s ?\n",nomeDaCidade1 );
     scanf("%f", &PopulacaoDaCidade1);
 
     printf("Pode me falar qual é a área (Em km²) de %s?\n",nomeDaCidade1);
@@ -39,12 +43,15 @@ int main(){
     printf("Quantos pontos Turisticos tem em %s?\n",nomeDaCidade1 );
     scanf(" %d", &NumerosDePontosTuristicosCidade1);
 
+    DensidadePopulacionalcidade1 = (PopulacaoDaCidade1 * 1e6) / AreaEmKmCidade1;
+    PIBpercapitacidade1 =  (PIBCidade1 *1e9 ) / (PopulacaoDaCidade1 * 1e6);
+
     // carta B
 
     printf("Agora vamos para a segunda carta, ela receberá o codigo de especificação: B01. \n\nDigite qual será o nome da sua cidade: \n");
-    scanf("%s", &nomeDaCidade2 );
+    scanf("%s[^\n]", nomeDaCidade2 );
 
-    printf("Qual é a população de abitantes (Em Milhões) de %s ?\n",nomeDaCidade2 );
+    printf("Qual é a população de habitantes (Em Milhões) de %s ?\n",nomeDaCidade2 );
     scanf("%f", &PopulacaoDaCidade2);
 
     printf("Pode me falar qual é a área (Em km²) de %s?\n",nomeDaCidade2);
@@ -56,6 +63,10 @@ int main(){
     printf("Quantos pontos Turisticos tem em %s?\n",nomeDaCidade2 );
     scanf(" %d", &NumerosDePontosTuristicosCidade2 );
 
+
+    DensidadePopulacionalcidade2 = (PopulacaoDaCidade2 *1e6) / AreaEmKmCidade2;
+    PIBpercapitacidade2 =  (PIBCidade2 *1e9 ) / (PopulacaoDaCidade2 * 1e6);
+
     
 
     // carta A
@@ -66,7 +77,9 @@ int main(){
     printf("População: %.2f Milhões \n", PopulacaoDaCidade1);
     printf("Área em KM²: %.2f KM² \n",AreaEmKmCidade1);
     printf("PIB: %.2f Bilhões \n",PIBCidade1);
-    printf("Números de pontos turisticos: %d \n\n",NumerosDePontosTuristicosCidade1);
+    printf("Números de pontos turisticos: %d \n",NumerosDePontosTuristicosCidade1);
+    printf("Densidade Populacional: %.2f hab/km². \n", DensidadePopulacionalcidade1);
+    printf("PIB per capita: %.2f reais.\n\n", PIBpercapitacidade1);
 
     // carta B
 
@@ -78,6 +91,8 @@ int main(){
     printf("Área em KM²: %.2f KM² \n",AreaEmKmCidade2);
     printf("PIB: %.2f Bilhões \n",PIBCidade2);
     printf("Números de pontos turisticos: %d \n",NumerosDePontosTuristicosCidade2);
+    printf("Densidade Populacional: %.2f hab/km². \n", DensidadePopulacionalcidade2);
+    printf("PIB per capita: %.2f reais.\n\n", PIBpercapitacidade2);
 
 
     
